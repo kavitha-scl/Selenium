93691d76955b8008fdea7fd6677b5960ba24ec51
kavitha-scl <kavi24mohan@gmail.com>
2025-05-02 13:10:53 +0800

    Add files via upload
    Datatypes-week1

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		float version = 91.0f;
+		System.out.println(version);
+		
+		String name="Google";
+		System.out.println(name);
+		
+		boolean type=false;
+		System.out.println(type);
+		
+		int a=2008;
+		System.out.println(a);
+		
+		char value='C';
+		System.out.println(value);
+	}
+
+}
